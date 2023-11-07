5c14ccc4cdb43fc3cd4b60a3a552126c7273837b
jrios93 <jcb.rios93@gmail.com>
2023-11-07 14:41:57 -0500

    responsive

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var toggleButton = document.getElementById("toggle-button");
-var body = document.querySelector(".container");
-var products = document.querySelectorAll(".card");
+let toggleButton = document.getElementById("toggle-button");
+let body = document.querySelector(".container");
+let products = document.querySelectorAll(".card");
 
 toggleButton.addEventListener("click", function () {
   body.classList.toggle("dark-mode");
 
   products.forEach(function (product) {
-    var card = product;
-    var tag = product.querySelector(".tag");
-    var textContainer = product.querySelector(".title-product");
+    let card = product;
+    let tag = product.querySelector(".tag");
+    let textContainer = product.querySelector(".title-product");
 
     card.classList.toggle("card-dark");
     tag.classList.toggle("tag-dark");
